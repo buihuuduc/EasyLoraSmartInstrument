11990842809fe4f1b9552d28c366bad38e5c0d83
buihuuduc <huuduc.electric@gmail.com>
2019-11-16 13:48:07 +0700

    arrangment

--- EasyLoraNode32/utils.ino
+++ EasyLoraNode32/utils.ino
@@ -1,3 +1,26 @@
+void setupSpeaker()
+{
+  // speaker
+  ledcSetup(SPK_CHANNEL, SPK_FREQ, SPK_RESOLUTION);
+  ledcAttachPin(SPK, SPK_CHANNEL);   
+  onSpeaker();
+  delay(100);
+  offSpeaker(); 
+}
+
+void onSpeaker()
+{
+  Serial.println("On Speaker");
+  //ledcWriteTone(SPK_CHANNEL, 2400);
+  ledcWrite(SPK_CHANNEL, SPK_FREQ);
+}
+
+void offSpeaker()
+{
+  Serial.println("Off Speaker");
+  ledcWrite(SPK_CHANNEL, 0);
+}
+
 // To get chip ID, ESP32 will return last 3 octets of mac address
 String getShortChipID()
 {  
